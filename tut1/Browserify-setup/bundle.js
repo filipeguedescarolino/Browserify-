85486b9b576b96fb086be993b3b904602277adca
1190100 <1190100@isep.ipp.pt>
2020-10-08 13:55:37 +0100

    final commit: -browserify with many sub components:  tinify + animejs + watchify + jquery

---
+++ tut1/Browserify-setup/bundle.js
@@ -0,0 +1,6 @@
+const uniq = require('uniq')
+const $ = require('jquery')
+
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+console.log(uniq(data));
---
+++ tut2/src/main.js
@@ -0,0 +1,11 @@
+const anime = require('animejs');
+// import anime from 'animejs';
+anime({
+    targets: 'body',
+    duration: 5000,
+    backgroundColor: "#ffea00"
+});
+const a = "hello B";
+console.log(a);
+const b = "hello a";
+console.log(b);
